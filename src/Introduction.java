b2063943c87786bc2b1af660b0481ce28e3ddce8
DunnTapCodee <dunnbxvp@gmail.com>
2025-09-24 23:31:39 +0700

    Accomplished HW1

---
+++ src/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+    public static void main(String[] args) {
+
+        System.out.println("Nguyen Trung Dung \t 24021434 \t K69I-CS5 \t nguyentrungdungbxvp \t 24021434@vnu.edu.vn");
+        for (int i = 9; i>0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.print("No more bottles on the wall.");
+    }
+}
